e4181d99a5c8414614002919f2d8b55c88232997
Michał Zakrzewski <michal.zakrzewsky@gmail.com>
2020-10-22 23:11:31 +0200

    first commit

---
+++ src/main/java/com/zakrzewski/intentionsbook/IntentionsBookApplication.java
@@ -0,0 +1,13 @@
+package com.zakrzewski.intentionsbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntentionsBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntentionsBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zakrzewski/intentionsbook/IntentionsBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zakrzewski.intentionsbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntentionsBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
